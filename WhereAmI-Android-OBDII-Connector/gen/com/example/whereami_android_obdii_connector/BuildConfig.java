1e4a670cd7c342791131cd476ec2ddef47ba0160
Pranavam Siddharthan <spranava@ford.com>
2016-06-09 12:45:02 +0530

    Initial Project

---
+++ WhereAmI-Android-OBDII-Connector/gen/com/example/whereami_android_obdii_connector/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.whereami_android_obdii_connector;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
